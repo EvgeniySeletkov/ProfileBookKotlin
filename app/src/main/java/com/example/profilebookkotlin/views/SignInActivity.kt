806cc8fc31aa21b22c41905934b2bad1d6b09f92
DESKTOP-POHC2RF\Jeka <evgeniy.seletkov@gmail.com>
2021-12-16 21:06:02 +0200

    first commit

---
+++ app/src/main/java/com/example/profilebookkotlin/views/SignInActivity.kt
@@ -0,0 +1,12 @@
+package com.example.profilebookkotlin.views
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.profilebookkotlin.R
+
+class SignInActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in)
+    }
+}
